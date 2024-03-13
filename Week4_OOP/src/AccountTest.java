38daa638834b6ca25f24ce23c081d2a598175755
Nguyen Duy Giang <158442283+ndgiang02@users.noreply.github.com>
2024-03-13 14:25:47 +0700

    Add files via upload

---
+++ Week4_OOP/src/AccountTest.java
@@ -0,0 +1,9 @@
+public class AccountTest {
+    public static void main(String []args) {
+        Account acc = new Account();
+        acc.addTransaction(2000.255,"deposit");
+        acc.addTransaction(1000,"withdraw");
+        acc.addTransaction(1000,"withdraw");
+        acc.printTransaction();
+    }
+}
